51c757499a594602ceb46e41eb71152e743a0849
ericsmslvdr <ericsmsalvador@gmail.com>
2024-04-05 13:26:34 +0800

    Move to folder

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { AuthGuard } from "@components/AuthGuard";
+import { AuthGuard } from "@components/guards/AuthGuard";
 import { ChatRoom } from "@pages/ChatRoom";
 import { Home } from "@pages/Home";
 import { createBrowserRouter } from "react-router-dom";
