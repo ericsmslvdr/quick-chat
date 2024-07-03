cc47b240f555e3c8c0941492ab51d0be64479425
ericsmslvdr <ericsmsalvador@gmail.com>
2024-07-03 19:56:36 +0800

    Initial commit

--- server/features/user/user-routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createUser, deleteUser } from './user-controllers.js'
-
-const router = express.Router()
-
-router.post('/auth', createUser)
-router.delete('/:id', deleteUser)
-
-export default router
