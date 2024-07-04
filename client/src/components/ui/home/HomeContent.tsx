e7001740270fd98969aecd16255e5f13e015dc09
ericsmslvdr <ericsmsalvador@gmail.com>
2024-07-04 20:48:06 +0800

    style: Center div

--- client/src/components/ui/home/HomeContent.tsx
+++ client/src/components/ui/home/HomeContent.tsx
@@ -1,8 +1,9 @@
+import style from './home.module.css';
 import CreateProfileForm from '../form/CreateProfileForm';
 
 const HomeDescription = () => {
     return (
-        <div>
+        <div className={style["home-container"]}>
             <h1>TaraChat</h1>
             <hr />
             <p>Welcome to TaraChat! <br />Please enter your information to get started.</p>
